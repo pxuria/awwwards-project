0c8c5a3d58cf34a0476e9d1ff3bf95b7a1ba5178
pxuria <pouriamesram@gmail.com>
2024-11-23 02:55:50 +0330

    init project v1

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <div className="relative h-dvh w-screen overflow-x-hidden">
+            <div id="video-frame" className="relative z-10 h-dvh overflow-hidden rounded-lg bg-blue-75">
+                <div>
+                    <div className="mask-clip-path"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
